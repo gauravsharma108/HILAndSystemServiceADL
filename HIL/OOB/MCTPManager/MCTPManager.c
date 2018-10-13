858e8110175d06d2bcfa499414a8381896763c7e
gauravsharma108 <gaurav52075@yahoo.com>
2018-10-13 16:48:53 +0530

    Adding OOB new feature

---
+++ HIL/OOB/MCTPManager/MCTPManager.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+printf("inside OOB branch in MCTP Manager\n");
+}
---
+++ HIL/OOB/MCTPPacketizer/MCTPPacketizer.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+printf("inside OOB branch in MCTP Packetizer\n");
+}
